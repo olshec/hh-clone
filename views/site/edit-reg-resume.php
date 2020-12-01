af8842c6d73388f46dd9cc7c826b072e776e889b
Oleg Shestakov <olshec@gmail.com>
2020-12-01 11:06:14 +0300

    set \yii\helpers\Url

--- views/site/edit-reg-resume.php
+++ views/site/edit-reg-resume.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 $this->params['listResumeState'] = $listResumeState;
-$this->params['resumeState'] = $resumeState;
+$this->params['resumeState'] = $resumeState; 
 $addressServer = Yii::$app->request->getHostInfo().Yii::$app->request->getBaseUrl().'/';
 
 $this->title = 'Редактирование резюме'

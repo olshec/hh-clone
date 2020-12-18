a4350718292be8e7cc7fce6d5d35e595208901e2
Oleg Shestakov <olshec@gmail.com>
2020-12-18 12:07:24 +0300

    renamed addresume -> addResume

--- migrations/m201204_130236_create_resume_table.php
+++ migrations/m201204_130236_create_resume_table.php
@@ -26,11 +26,11 @@ class m201204_130236_create_resume_table extends Migration
         $this->addForeignKey('fk_resume_user_id', 'resume', 'user_id', 
             'user', 'id', 'cascade', 'cascade');
 
-        $this->addresume('Anton', 'Lavrov', '1990-02-18', 'Manager', 100, 'about me');
-        $this->addresume('Anna', 'Mironova', '1992-11-25', 'Director' ,200, 'about me');
+        $this->addResume('Anton', 'Lavrov', '1990-02-18', 'Manager', 100, 'about me');
+        $this->addResume('Anna', 'Mironova', '1992-11-25', 'Director' ,200, 'about me');
     }
     
-    public function addresume(string $nameUser, string $surnameUser, string $dateBirth, string $nameResume, int $salary, string $aboutMe) {
+    public function addResume(string $nameUser, string $surnameUser, string $dateBirth, string $nameResume, int $salary, string $aboutMe) {
         $command = Yii::$app->db->createCommand('SELECT * FROM "user" WHERE name=:name AND surname=:surname AND date_birth=:date_birth');
         $command->bindValue(':name', $nameUser);
         $command->bindValue(':surname', $surnameUser);

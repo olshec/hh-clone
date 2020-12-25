35fe73126373f0f279de094d06acb8916ae91add
Oleg Shestakov <olshec@gmail.com>
2020-12-25 20:32:58 +0300

    fixed serch city with gender

--- web/js/my_script.js
+++ web/js/my_script.js
@@ -1,3 +1,8 @@
+
+sercherUrl =  "http://localhost/hh-clone/web/resume/?";
+city = '';
+gender = '';
+
 class City {
     constructor(idCity) {
          this.setIdCity(idCity);
@@ -9,12 +14,13 @@ class City {
         return this.idCity;
     }
     
-    serchIdCity(idCitySerch) {
-        if (this.idCity != idCitySerch) {
-            window.location.href = "http://localhost/hh-clone/web/resume/?"+"gender="+gender.getName()+"&city="+idCitySerch;
-        }
+    addSerchParams(idCitySerch) {
+		city = new City(idCitySerch);
+		sercherUrl += "city="+idCitySerch;
+        //window.location.href = "http://localhost/hh-clone/web/resume/?"+"gender="+gender.getName()+"&city="+idCitySerch;
     }
 }
+
 
 class Gender {
 	constructor(genderName) {
@@ -26,6 +32,10 @@ class Gender {
 	getName() {
 		return this.genderName;
 	}
+	addSerchParams(genderName) {
+		gender = new Gender(genderName);
+		sercherUrl += "&gender="+genderName;
+    }
 }
 
 function afterPageLoad() {
@@ -46,17 +56,30 @@ function afterPageLoad() {
 }
 
 function serchCity() {
-    elements = document.getElementsByClassName("nselect-1");
-    idCitySerch = elements[0].value;
-    city.serchIdCity(idCitySerch);
+	//city.addSerchParams(idCitySerch);
+	elements = document.getElementsByClassName("nselect-1");
+	idCitySerch = elements[0].value;
+	if (city.getIdCity() != idCitySerch){
+	    city.addSerchParams(idCitySerch);
+		gender.addSerchParams(gender.getName());
+		window.location.href = sercherUrl;
+	}
+	
 }
 
+function addCityParams() {
+	elements = document.getElementsByClassName("nselect-1");
+	idCitySerch = elements[0].value;
+	sercherUrl += "city="+idCitySerch;
+}
 
 function serchGender(genderName) {
-	if(genderName != gender.getName){
-		gender = new Gender(genderName);
-    	window.location.href = "http://localhost/hh-clone/web/resume/?"+"gender="+gender.getName()+"&city="+city.getIdCity();
+	if(genderName != gender.getName()){
+		addCityParams();
+		gender.addSerchParams(genderName);
+		window.location.href = sercherUrl;
 	}
+	
 }
 
 

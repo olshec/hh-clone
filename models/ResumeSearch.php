e55101bce4824dcdecf327b35a97d5e7bdae0948
Oleg Shestakov <olshec@gmail.com>
2020-12-27 16:28:30 +0300

    fixed serch by gender

--- models/ResumeSearch.php
+++ models/ResumeSearch.php
@@ -4,6 +4,7 @@ namespace app\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
+use yii\db\ActiveQuery;
 use app\models\Resume;
 use Yii;
 
@@ -31,7 +32,67 @@ class ResumeSearch extends Resume
         // bypass scenarios() implementation in the parent class
         return Model::scenarios();
     }
+    
+    private function getCityId(ActiveQuery $query, array $params): ActiveQuery{
+        if(array_key_exists('cityId', $params)) {
+            $cityId = $params['cityId'];
+            if($cityId != 0){
+                $query->innerJoin('city', '"user"."city_id" = ' . $cityId);
+            }
+        }
+        return $query;
+    }
 
+    private function getSpecializationId(ActiveQuery $query, array $params): ActiveQuery{
+        if(array_key_exists('idSpecialization', $params)) {
+            $idSpecialization = $params['idSpecialization'];
+            if($idSpecialization != 0){
+                $query->innerJoin('place_of_work', '"resume"."id" = ' . '"place_of_work"."resume_id"');
+                $query->where = '"place_of_work"."specialization_id" = \''.$idSpecialization.'\'';
+            }
+        }
+        return $query;
+    }
+    
+    private function getListTypeEmployments(ActiveQuery $query, array $params): ActiveQuery{
+        $where = $query->where;
+        $hasParentheses = false;
+        if(array_key_exists('listTypeEmployments', $params)) {
+            $listTypeEmployments = $params['listTypeEmployments'];
+            if($where != '') {
+                $where .= ' AND (';
+                $hasParentheses = true;
+            }
+            $query->innerJoin('resume_type_employment', '"resume_type_employment"."resume_id" = ' . '"resume"."id"');
+            
+            for($i=0; $i<count($listTypeEmployments); $i++) {
+                if($i>=1 && $i<count($listTypeEmployments)) {
+                    $or = ' OR ';
+                }
+                else {
+                    $or = ' ';
+                }
+                $where .= $or.'"resume_type_employment"."type_employment_id" = \''.$listTypeEmployments[$i].'\'';
+            }
+            if($hasParentheses == true){
+                $where .= ') ';
+            }
+        }
+        $query->where = $where;
+        return $query;
+    }
+    
+    private function getGender(ActiveQuery $query, array $params): ActiveQuery{
+        $gender = $params['gender'];
+        if($gender != 'all') {
+            if($query->where != '') {
+                $query->where = '('.$query->where.') AND ';
+            }
+            $query->where .= '"user"."gender" = \''.$gender.'\'';
+        }
+        return $query;
+    }
+    
     /**
      * Creates data provider instance with search query applied
      *
@@ -41,63 +102,24 @@ class ResumeSearch extends Resume
      */
     public function search($params)
     {
-        $gender = $params['gender'];
+       
         $query = Resume::find()
         ->innerJoin('user', '"resume"."user_id" = "user"."id"');
         $orderType = $params['orderType'] == 'DESC'? SORT_DESC:SORT_ASC;
-        if(array_key_exists('cityId', $params)) {
-            $cityId = $params['cityId'];
-            if($cityId != 0){
-                $query->innerJoin('city', '"user"."city_id" = ' . $cityId);
-            }
-        }
         
-        $where = '';
-        if(array_key_exists('idSpecialization', $params)) {
-            $idSpecialization = $params['idSpecialization'];
-            if($idSpecialization != 0){
-                $query->innerJoin('place_of_work', '"resume"."id" = ' . '"place_of_work"."resume_id"');
-                $where = '"place_of_work"."specialization_id" = \''.$idSpecialization.'\'';
-            }
-        }
-        
-        $hasParentheses = false;
-        if(array_key_exists('listTypeEmployments', $params)) {
-            $listTypeEmployments = $params['listTypeEmployments'];
-            if($where != '') {
-                $where .= ' AND (';
-                $hasParentheses = true;
-            }
-            $query->innerJoin('resume_type_employment', '"resume_type_employment"."resume_id" = ' . '"resume"."id"');
+        $query = $this->getCityId($query, $params);
+        $query = $this->getSpecializationId($query, $params);
+        $query = $this->getlistTypeEmployments($query, $params);
+        $query = $this->getGender($query, $params);
 
-            for($i=0; $i<count($listTypeEmployments); $i++) {
-                if($i>=1 && $i<count($listTypeEmployments)) {
-                    $or = ' OR ';
-                }
-                else {
-                    $or = ' ';
-                }
-                $where .= $or.'"resume_type_employment"."type_employment_id" = \''.$listTypeEmployments[$i].'\''; 
-            }
-            if($hasParentheses == true){
-                $where .= ') ';
-            }
-        }
         //$listCheckBoxSchedules
         
-//         print_r($where);
-//         exit();
+
         
-        if($gender != 'all') {
-            if($where != '') {
-                $where .= ' AND ';
-            } 
-            $where .= '"user"."gender" = \''.$gender.'\''; 
-        }
-        
-        
-        $query->where($where);
        
+       // $query->where($query->where);
+//         print_r($query->where);
+//         exit();
         
         $query->orderBy([$params['orderTable'] => $orderType]);
         

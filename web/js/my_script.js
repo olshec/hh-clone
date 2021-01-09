b478ef75c32fa41e3edd88f3702bc26cfcdc384b
Oleg Shestakov <olshec@gmail.com>
2021-01-09 19:29:41 +0300

    fixed js full text serch

--- web/js/my_script.js
+++ web/js/my_script.js
@@ -603,8 +603,8 @@ el.addEventListener("click", serchFullText, false);
 
 el = document.getElementById("full-text-serch");
 el.addEventListener('keydown', function(e) {
-	e.preventDefault();
     if (e.keyCode === 13) {
+		e.preventDefault();
     	serchFullText();
     }
   }, false);
